eda7596b30c68301999873cadb0702620e52d84e
sota <mtnsot919@icloud.com>
2024-01-16 09:44:48 +0000

    課題9

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,14 @@ use App\Http\Controllers\Admin\NewsController;
 Route::controller(NewsController::class)->prefix('admin')->group(function() {
     Route::get('news/create', 'add');
 });
+
+
+//「http://XXXXXX.jp/XXX というアクセスが来たときに、 AAAControllerのbbbというAction に渡すRoutingの設定」を書いてみてください
+//   Route::get('XXX', 'AAAController@bbb' );
+
+
+// 前章でAdmin/ProfileControllerを作成し、add Action, edit Actionを追加しました。web.phpを編集して、admin/profile/create にアクセスしたら ProfileController の add Action に、admin/profile/edit にアクセスしたら ProfileController の edit Action に割り当てるように設定してください
+   
+    Route::get('admin/profile/create','Admin\ProfileController@add');
+    
+    Route::get('admin/profile/edit','Admin\ProfileController@edit');
