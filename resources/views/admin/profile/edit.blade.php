96a81eab7a8cf781ac8ed197b64fd2b8cb73afd3
sota <mtnsot919@icloud.com>
2024-01-19 05:46:41 +0000

    課題10修正

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -3,14 +3,14 @@
 
 
 {{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
-@section('title', 'プロフィールの新規登録ページ')
+@section('title', 'プロフィールの編集ページ')
 
 {{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
 @section('content')
     <div class="container">
         <div class="row">
             <div class="col-md-8 mx-auto">
-                <h2>プロフィールの新規登録ページ</h2>
+                <h2>プロフィールの編集ページ</h2>
             </div>
         </div>
     </div>
