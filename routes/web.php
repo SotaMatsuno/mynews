6c7d712f606c76b3e0058da5005da22cc1642e55
sota <mtnsot919@icloud.com>
2024-01-19 05:36:04 +0000

    課題9修正

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
 
 use App\Http\Controllers\Admin\NewsController;
 Route::controller(NewsController::class)->prefix('admin')->group(function() {
-    Route::get('news/create', 'add') ->middleware('auth');
+    Route::get('news/create', 'add')->middleware('auth');
 });
 
 
@@ -28,13 +28,12 @@ Route::controller(NewsController::class)->prefix('admin')->group(function() {
 
 
 // 前章でAdmin/ProfileControllerを作成し、add Action, edit Actionを追加しました。web.phpを編集して、admin/profile/create にアクセスしたら ProfileController の add Action に、admin/profile/edit にアクセスしたら ProfileController の edit Action に割り当てるように設定してください
-   
-    Route::get('admin/profile/create','Admin\ProfileController@add');
-    
-    Route::get('admin/profile/edit','Admin\ProfileController@edit');
-Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+use App\Http\Controllers\Admin\ProfileController;
+Route::controller(ProfileController::class)->prefix('admin')->group(function() {
+    Route::get('profile/create', 'add')->middleware('auth');
+    Route::get('profile/edit', 'edit')->middleware('auth');
+});
 
 Auth::routes();
 
