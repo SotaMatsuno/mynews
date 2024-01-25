cbcc8da148f7475798a2c38bcdaf5282c69ee171
sota <mtnsot919@icloud.com>
2024-01-25 14:20:37 +0000

    課題12

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -11,6 +11,8 @@ class ProfileController extends Controller
     public function add()
     {
         return view('admin.profile.create');
+        
+        return redirect('admin/profile/create');
     }
 
     public function create()
@@ -21,6 +23,8 @@ class ProfileController extends Controller
     public function edit()
     {
         return view('admin.profile.edit');
+        
+        return redirect('admin/profile/edit');
     }
 
     public function update()
--- webpack.mix.js
+++ webpack.mix.js
@@ -14,11 +14,8 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .sass('resources/sass/admin.scss', 'public/css')
+    .sass('resources/sass/profile.scss', 'public/css')
     .sourceMaps();
     
 
-// webpack.mix.jsを編集して、profile.scss をコンパイルするように編集する
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css')
-    .sass('resources/sass/profile.scss', 'public/css')
-    .sourceMaps();
+// 課題11-6 webpack.mix.jsを編集して、profile.scss をコンパイルするように編集する
